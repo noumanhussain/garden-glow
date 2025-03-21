6ae5bc810336463f2733fdbf3264779318606bde
noumanhussain <noumanzakar@gmail.com>
2025-03-22 01:36:18 +0500

    react with inertia setup

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-gray-900">
+                Welcome to Laravel + React + Inertia
+            </h1>
+        </div>
+    );
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,23 @@
+import "./bootstrap";
+import "../css/app.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+createInertiaApp({
+    title: (title) => `${title} - My App`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob("./Pages/**/*.jsx")
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+        root.render(
+            <React.StrictMode>
+                <App {...props} />
+            </React.StrictMode>
+        );
+    },
+});
